972ac7b7ff20c3b911436d1fdf21a2051b7d3590
Florin Pop <popflorin1705@yahoo.com>
2017-01-29 17:13:23 +0200

    socket connection from frontend

--- app.js
+++ app.js
@@ -21,8 +21,9 @@ io.sockets.on('connection', function(socket){
     connections.push(socket);
     console.log('Connected: %s sockets connected.', connections.length);
     
-    io.socket.on('disconnect', function(){
+    //Disconnect
+    socket.on('disconnect', function(data){
         connections.splice(connections.indexOf(socket), 1);
-        console.log('Disconnected: %s sockets connected.', connection.length);
+        console.log('Disconnected: %s sockets connected.', connections.length);
     });
 });
