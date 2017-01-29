32759a998701054a0af808f27410c96cf265694f
Florin Pop <popflorin1705@yahoo.com>
2017-01-29 17:01:03 +0200

    running

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ var io = require('socket.io').listen(server);
 var users = [];
 var connections = [];
 var PORT = process.env.PORT || 3000;
+
+var path = "/static";
+
 server.listen(PORT, function(){
     console.log("Server running on port", PORT);
 });
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + path + '/index.html');
 });
