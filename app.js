095b9e1e6f33fc0de6f60e687aaae0abe99e6fd8
Florin Pop <popflorin1705@yahoo.com>
2017-01-29 17:33:27 +0200

    get users

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ io.sockets.on('connection', function(socket){
     
     //Disconnect
     socket.on('disconnect', function(data){
+        if(!socket.username) return;
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected.', connections.length);
     });
@@ -32,4 +35,16 @@ io.sockets.on('connection', function(socket){
         console.log(data);
         io.sockets.emit('new message', {msg: data}); 
     });
+    
+    //New user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    })
+    
+    function updateUsernames(){
+        io.sockets.emit('get users', user);
+    }
 });
